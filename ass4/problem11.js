719e688d64ae01fb71efe011f6fd78c623c5f721
kovvuri jagadish reddy <jagadishreddykovvuri@gmail.com>
2019-06-29 17:38:45 +0530

    problem11 added

--- ass4/problem11.js
+++ ass4/problem11.js
@@ -1,95 +1,104 @@
-function check(obj){
+function check(arr,obj){
     var k=-1
-        for(var i=0;i<showroomcars.length-1;i++){
-            
-            if(showroomcars[i].id===obj){
+        for(var i=0;i<arr.length;i++){
+            if(arr[i].id===obj){
                 k=i
+
                 break;
             }
         }
             return k;
-
 }
-var cars=[ {
-    id:1,
-    color:"red",
-    speed:"100bhp",
-    fuel:"petrol"
-},
-{
-    id:2,
-    color:"blue",
-    speed:"150bhp",
-    fuel:"gas"
-},
-{
-    id:3,
-    color:"black",
-    speed:"200bhp",
-    fuel:"disel"
-}]
+var cars=[
+     {
+         id:1,
+         color:"red",
+         speed:"100bhp",
+         fuel:"petrol"
+        },
+        {
+             id:2,
+             color:"blue",
+             speed:"150bhp",
+             fuel:"gas"
+            },
+            {
+                id:3,
+                color:"black",
+                speed:"200bhp",
+                fuel:"disel"
+            },
+            {
+                id:4,
+                color:"black",
+                speed:"200bhp",
+                fuel:"disel"
+            },
+            {
+                id:5,
+                color:"black",
+                speed:"200bhp",
+                fuel:"disel"
+            },
+            {
+                id:6,
+                color:"black",
+                speed:"200bhp",
+                fuel:"disel"
+            },
+            {
+                id:7,
+                color:"black",
+                speed:"200bhp",
+                fuel:"disel"
+            }
+]
+var a={ 
+     Firstname:"jagadish",
+     Lastname:"reddy",
+     Wishlist:[],
+     ownedlist:[],
+     Wishlist1:function (arr){
+        if(a.Wishlist.indexOf(arr)==-1){
+            a.Wishlist.push(arr)
+            console.log("car added to wishlist")}
+        else{
+            console.log("car not added to wishlist") 
+        }} ,
 
-
-
-var showroomcars=[
-    {
-        id:1,
-        color:"red",
-        speed:"100bhp",
-        fuel:"petrol"
-    },
-    {
-        id:2,
-        color:"blue",
-        speed:"150bhp",
-        fuel:"gas"
-    },
-    {
-        id:3,
-        color:"black",
-        speed:"200bhp",
-        fuel:"disel"
-    },
-    {sellcar: function (obj){
-        var k=check(obj)
+    buycar: function (arr,obj){
+            var k=check(arr.carslist,obj)
         if(k==-1){
-            console.log("that car not avaliable")
+             console.log("that car not avaliable")
+        }   
+    else{
+        if(a.Wishlist.indexOf(cars[arr.carslist[k].id-1])==-1){
+            console.log("first add to wishlist")
         }
         else{
-            showroomcars.splice(k, 1)
-            console.log(showroomcars)
+            a.ownedlist.push(arr.carslist[k])
+            console.log("required car owned")
+            showroom.sellcar(obj)
+            var r=a.Wishlist.indexOf(arr.carslist[k])
+            a.Wishlist.splice(r,1)
         }
-    
+    }}
+  }
+var showroom={
+    carslist:[{id:1,color:"red",speed:"100bhp",fuel:"petrol"},{id:2,color:"blue",speed:"150bhp",fuel:"gas"},{id:3,color:"black",speed:"200bhp",fuel:"disel"}],
+    getcar:function(obj){
+    showroom.carslist=[obj].concat(showroom.carslist)
+    console.log("get car")
+    },
+    sellcar: function (obj){
+       var k=check(showroom.carslist,obj)
+       showroom.carslist.splice(k,1)
+       }
     }
-}
-]
-var a={ Firstname:"jagadish", Lastname:"reddy",Wishlist:[],ownedlist:[],visitedcar:function (obj){
-    var k=check(obj)
-    if(k==-1){
-        console.log("that car not avaliable")
-    }
-    else{
-    a.Wishlist.push(showroomcars[k])
-    console.log(a)}
-   
-} , buycar: function (obj){
-    var k=check(obj)
-    if(k==-1){
-        console.log("that car is not avaliable")
-    }else{
-    a.ownedlist.push(showroomcars[k])
-    console.log(a)}
+var showroom1={carslist:[]}
+a.Wishlist1(cars[2])
+a.Wishlist1(cars[2])
+a.buycar(showroom,3)
+showroom.getcar(cars[2])
+a.buycar(showroom,3)
 
-}}
-
-
-
-
-
-
-a.visitedcar(1)
-a.buycar(4)
-showroomcars[3].sellcar(4)
-
-//console.log("      ")
-//console.log(showroomcars)
