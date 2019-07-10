f9a0c722c92aebf2a296906b788c03bf076f3fad
kovvuri jagadish reddy <jagadishreddykovvuri@gmail.com>
2019-07-10 14:24:37 +0530

    stock ass7

---
+++ stock/src/FilterableProductTable/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './styles.css'
+import SearchBar from "./SearchBar"
+import ProductTable from "./ProductTable"
+class FilterableProductTable extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked:false,
+            searchValue:''
+        };
+        }
+    
+    filtering=(searchword)=>{
+        
+        this.setState({
+            searchValue:searchword
+        })
+
+    }
+    checker=()=>{
+        this.setState({
+            checked:!this.state.checked
+        })
+    }
+
+
+    render()
+    {   
+        var result=this.props.products
+        if(this.state.checked){
+            result=result.filter(item =>item.stocked===true)
+        }
+        result=result.filter(item =>item.name.includes(this.state.searchValue))
+        console.log(result)
+        return(
+            <>
+            <div>
+            <SearchBar getInput={this.filtering} checking={this.checker}/>
+            <ProductTable products={result} />
+            </div>
+            </>
+        )
+    }
+
+
+}
+export default  FilterableProductTable
